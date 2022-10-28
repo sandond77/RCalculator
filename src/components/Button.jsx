967a484e72a51dc5e925cd89c6aa3e9aeae3b144
unknown <sandu@ucdavis.edu>
2022-10-27 23:33:04 -0700

    add logic to handle button input and first set of values

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
 export default function Button(props) {
+  function handleClick(event){
+    const val = isNaN(parseInt(event.target.value)) ? event.target.value : parseInt(event.target.value);
+    console.log(val)
+
+    if(typeof(val) === "number"){
+      console.log('num');
+      let concat;
+      if(props.firstValues === 0){
+        concat = val
+      } else {
+        concat = props.firstValues.toString();
+        concat += val;
+      }
+      props.setFirstValues(parseInt(concat));
+    } else if(val) {
+      console.log('not num');
+    }
+  }
   return (
-    <button type="button" className="btn btn-rounded btn-sm" value={props.value}>{props.value}</button>
+    <button 
+      type="button" 
+      className="btn btn-rounded btn-sm" 
+      value={props.value}
+      onClick={handleClick}
+    >
+      {props.value}
+    </button>
   )
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,19 +2,20 @@ import React, {useState} from 'react'
 import Button from './Button';
 
 export default function Calculator() {
-  const values = [7, 8 , 9, "/", 4, 5, 6, "-", 1, 2, 3, "+", "+/-", 0, ".", "="]
-  const [firstValues, setFirstValues] = useState("");
+  const values = [7, 8 , 9, "/", 4, 5, 6, "-", 1, 2, 3, "+", "Clear", 0, ".", "="]
+  const [firstValues, setFirstValues] = useState(0);
   return (
     <div className='container'>
         <div className='display d-flex justify-content-center text-center'>
-            {}
+            {firstValues}
         </div>
         <div className='buttons-area justify-content-center text-center'>
             {values.map((value, i) =>
               <Button 
                 value = {value}
                 key = {i}
-                setValue = {setFirstValues};
+                firstValues = {firstValues}
+                setFirstValues = {setFirstValues}
               />
             )}
         </div>
